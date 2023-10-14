a98392c2042e727870fcf7871597a86cdf0d9ad5
Asnoyon <blackpain1100@gmail.com>
2023-10-14 20:13:09 +0600

    form validation with controlled and Uncontrolled

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+//uncontrolled component form
+
+import { useState } from "react";
+
+// import { useRef } from "react";
+// function App() {
+//   const nameRef = useRef();
+//   const emailRef = useRef();
+//   const passwordRef = useRef();
+//   const handleclick = (e) => {
+//     e.preventDefault();
+//     console.log(nameRef.current.value);
+//     console.log(emailRef.current.value);
+//     console.log(passwordRef.current.value);
+//   };
+//   return (
+//     <div className="App">
+//       <form onSubmit={handleclick}>
+//         <label>
+//           Name:
+//           <input type="text" name="name-input" ref={nameRef} />
+//         </label>
+//         <br />
+//         <label>
+//           Email:
+//           <input type="email" name="email-input" ref={emailRef} />
+//         </label>
+//         <label>
+//           <br />
+//           Password:
+//           <input type="password" name="password-input" ref={passwordRef} />
+//         </label>
+//         <br />
+//         <input type="submit" value="Submit" />
+//       </form>
+//     </div>
+//   );
+// }
+// export default App;
+
+//Controlled component in Form
 
 function App() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(name, email, password);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name:
+          <input
+            type="text"
+            name="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Email:
+          <input
+            type="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
     </div>
   );
 }
-
 export default App;
